6acf2d612765c8341e90117fc4e2dde5a5900899
Hasan Mufdy <h.mufdy@gmail.com>
2025-06-26 14:23:43 +0300

    Add project files.

---
+++ VehicleLookup/Controllers/CarLookupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleLookup.Controllers
+{
+    public class CarLookupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
